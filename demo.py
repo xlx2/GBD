670bae3c75bcedd0f791d892a8d7cc3cc1baa417
xlx2 <751333411@qq.com>
2024-12-10 12:18:07 +0800

    clear demo

--- demo.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-from utils import create_block_diag_matrix
-
-H = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
-print(np.hstack([np.zeros([2, 3]), np.array([[np.sqrt(2)],[1]])]))
