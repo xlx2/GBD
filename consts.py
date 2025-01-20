005dd824b98fb3cea463711cf3add4a976ef935c
xlx2 <751333411@qq.com>
2025-01-20 15:01:02 +0800

    update

--- consts.py
+++ consts.py
@@ -1,4 +1,4 @@
-from utils import dB2pow
+from utils import dB2pow, dBm2pow
 from fluid_antenna_system import FluidAntennaSystem
 import numpy as np
 
@@ -10,31 +10,35 @@ NUM_OF_ANTENNAS: int = 16
 NUM_OF_SELECTED_ANTENNAS: int = 10
 CHANNEL_NOISE: float = dB2pow(0)
 SENSING_NOISE: float = dB2pow(10)
-REFLECTION_COEFFICIENT: float = dB2pow(0)
+REFLECTION_COEFFICIENT: complex = 1.5 + 1j*1.5
 QOS_THRESHOLD: float = dB2pow(12)
 EVE_SENSING_THRESHOLD: float = dB2pow(12)
 CRB_THRESHOLD: float = dB2pow(0)
-DOA: np.ndarray = np.array([[np.pi / 6]])
+DOA: np.ndarray = np.array([[-np.pi / 6, 0]])
 indices: np.ndarray = np.arange(0, (NUM_OF_ANTENNAS - 1) + 1).reshape(-1, 1)
 STEERING_VECTOR: np.ndarray = np.exp(-1j * np.pi * indices * np.sin(DOA))
 DIFF_STEERING_VECTOR: np.ndarray = -1j * np.pi * indices * np.cos(DOA) * STEERING_VECTOR
+SNAPSHOT: int = 10
 
-fas = FluidAntennaSystem(num_of_yaxis_antennas=NUM_OF_ANTENNAS, num_of_users=NUM_OF_USERS,noise_variance=CHANNEL_NOISE)
+fas = FluidAntennaSystem(num_of_yaxis_antennas=NUM_OF_ANTENNAS,
+                         num_of_users=NUM_OF_USERS,
+                         noise_variance=CHANNEL_NOISE)
 CHANNEL = fas.get_channel()
-print(CHANNEL)
+
 
 Parameters = {
-    "numOfAntennas": NUM_OF_ANTENNAS,
+    "num_of_antennas": NUM_OF_ANTENNAS,
     "num_of_users": NUM_OF_USERS,
-    "numOfSelectedAntennas": NUM_OF_SELECTED_ANTENNAS,
-    "qosThreshold": QOS_THRESHOLD,
-    "eveSensingThreshold": EVE_SENSING_THRESHOLD,
-    "crbThreshold": CRB_THRESHOLD,
-    "reflectionCoefficient": REFLECTION_COEFFICIENT,
-    "channelNoise": CHANNEL_NOISE,
-    "sensingNoise": SENSING_NOISE,
+    "num_of_selected_antennas": NUM_OF_SELECTED_ANTENNAS,
+    "qos_threshold": QOS_THRESHOLD,
+    "eve_sensing_threshold": EVE_SENSING_THRESHOLD,
+    "crb_threshold": CRB_THRESHOLD,
+    "reflection_coefficient": REFLECTION_COEFFICIENT,
+    "channel_noise": CHANNEL_NOISE,
+    "sensing_noise": SENSING_NOISE,
     "doa": DOA,
-    "steeringVector": STEERING_VECTOR,
-    "diffSteeringVector": DIFF_STEERING_VECTOR,
-    "channel": CHANNEL
+    "steering_vector": STEERING_VECTOR,
+    "diff_steering_vector": DIFF_STEERING_VECTOR,
+    "channel": CHANNEL,
+    "snapshot": SNAPSHOT,
 }
---
+++ solvers.py
@@ -0,0 +1,193 @@
+import numpy as np
+import cvxpy as cp
+from consts import Parameters
+from utils import create_boolean_vector, create_block_diag_matrix
+
+
+class GBDSolver:
+    def __init__(self, parameters):
+        self.H = parameters['channel']  # Channel Matrix (N x K)
+        self.K = parameters['num_of_users']  # User Number
+        self.N = parameters['num_of_antennas']  # Port Number
+        self.N_sel = parameters['num_of_selected_antennas']  # Selected Port Number
+        self.gamma = parameters['qos_threshold']  # QoS Threshold
+        self.snr_eve = parameters['eve_sensing_threshold']  # Sensing Threshold
+        self.snr_crb = parameters['crb_threshold']  # CRB Threshold
+        self.sigmaC2 = parameters['channel_noise']  # Channel Noise
+        self.sigmaR2 = parameters['sensing_noise']  # Sensing Noise
+        self.beta = parameters['reflection_coefficient']
+        self.theta = parameters['doa']  # DOA (1 x M)
+        a = parameters['steering_vector']
+        a_diff = parameters['diff_steering_vector']
+        self.a_eve = a[:, 0:1]
+        self.a_target = a[:, 1:2]
+        self.a_eve_diff = a_diff[:, 0:1]
+        self.a_target_diff = a_diff[:, 1:2]
+        self.L = parameters['snapshot']
+
+        self.feasibilityCut = {
+            'W': [],
+            'miu': []
+        }
+        self.infeasibilityCut = {
+            'W': [],
+            'zeta': [],
+            'alpha': []
+        }
+
+    def prime_problem_solver(self, lambda_old=None, verbose=False) -> (bool, float):
+        # Initialize Boolean Variable
+        if lambda_old is None:
+            lambda_old = create_boolean_vector(self.N, self.N_sel)
+
+        # Define CVXPY Variables
+        W = cp.Variable((self.N, self.K + self.N), complex=True)
+        Q = cp.Variable((self.N, self.K + self.N), complex=True)
+        # t = cp.Variable(1, complex=False)
+        # Constraints
+        constraints = []
+
+        # Qos Constraint
+        for k in range(self.K):
+            lhs = np.sqrt(1 + 1 / self.gamma) * cp.real(self.H[:, k:k + 1].T @ np.diag(lambda_old[:, 0]) @ W[:, k:k + 1])
+            rhs = cp.norm(np.vstack(
+                [create_block_diag_matrix((self.H[:, k:k + 1].T @ np.diag(lambda_old[:, 0])).T, repeat=self.K + self.N),
+                 np.zeros([1, self.N * (self.K + self.N)])]) @
+                          W.reshape((W.size, 1), 'F') +
+                          np.vstack([np.zeros([self.K + self.N, 1]), np.array([[np.sqrt(self.sigmaC2)]])]), 2)
+            constraints.append(rhs - lhs <= 0)
+
+        # Q = Lambda * W
+        constraints.append(Q == np.diag(lambda_old[:, 0]) @ W)
+
+        # Eve Sensing SNR Constraint
+        constraints.append(np.abs(self.beta)**2 * cp.sum_squares(np.conj(self.a_eve.T) @ np.diag(lambda_old[:, 0]) @ W)
+                           - self.snr_eve * self.sigmaR2 <= 0)
+
+        # Imaging Part Constraint
+        for k in range(self.K):
+            constraints.append(cp.imag(self.H[:, k:k + 1].T @ np.diag(lambda_old[:, 0]) @ W[:, k:k + 1]) == 0)
+
+        # Define the objective function
+        objective = cp.Minimize(cp.sum_squares(np.diag(lambda_old[:, 0]) @ W))
+
+        # Solve the problem
+        problem = cp.Problem(objective, constraints)
+        problem.solve(solver=cp.MOSEK, verbose=verbose)
+
+        # Save the results
+        if problem.status == cp.OPTIMAL:
+            feasibility = True
+            miu = np.array(
+                [constraints[k].dual_value for k in range(self.K)]).reshape(-1, 1)
+            self.feasibilityCut['W'].append(W.value)
+            self.feasibilityCut['miu'].append(miu)
+        else:
+            feasibility = False
+
+        return feasibility, problem.value
+
+    def feasibility_check_problem_solver(self, lambda_old=None, verbose=False) -> bool:
+        # Initialize Boolean Variable
+        if lambda_old is None:
+            lambda_old = create_boolean_vector(self.N, self.N_sel)
+
+        # Define CVXPY Variables
+        W = cp.Variable((self.N, self.K + self.N), complex=True)
+        Wc = W[:, 0:self.K]
+        Wr = W[:, self.K:self.K + self.N]
+        alpha = cp.Variable((self.K, 1))
+
+        # Constraints
+        constraints = []
+
+        # Qos Constraint
+        for k in range(self.K):
+            lhs = np.sqrt(1 + 1 / self.gamma) * cp.real(
+                self.H[:, k:k + 1].T @ np.diag(lambda_old[:, 0]) @ Wc[:, k:k + 1])
+            rhs = cp.norm(np.vstack(
+                [create_block_diag_matrix((self.H[:, k:k + 1].T @ np.diag(lambda_old[:, 0])).T, repeat=self.K),
+                 np.zeros([1, self.N * self.K])]) @
+                          Wc.reshape((Wc.size, 1), 'F') +
+                          np.vstack([np.zeros([self.K, 1]), np.array([[np.sqrt(self.sigmaC2)]])]), 2)
+            constraints.append(rhs - lhs <= alpha[k])
+
+        for k in range(self.K):
+            constraints.append(cp.imag(self.H[:, k:k + 1].T @ np.diag(lambda_old[:, 0]) @ Wc[:, k:k + 1]) == 0)
+
+        # Non-negative Constraint
+        for k in range(self.K):
+            constraints.append(alpha[k] >= 0)
+
+        # Define the objective function
+        objective = cp.Minimize(cp.sum(alpha))
+
+        # Solve the problem
+        problem = cp.Problem(objective, constraints)
+        problem.solve(solver=cp.MOSEK, verbose=verbose)
+
+        # Save the results
+        if problem.status == cp.OPTIMAL:
+            feasibility = True
+            zeta = np.array(
+                [constraints[k].dual_value for k in range(self.K)]).reshape(-1, 1)
+            self.infeasibilityCut['W'].append(W.value)
+            self.infeasibilityCut['zeta'].append(zeta)
+            self.infeasibilityCut['alpha'].append(alpha.value)
+        else:
+            feasibility = False
+
+        return feasibility
+
+    def master_problem_solver(self, verbose=False) -> (np.ndarray, float):
+        # Initialize Boolean Variable
+        lambda_old = cp.Variable((self.N, 1), boolean=True)
+        eta = cp.Variable()
+
+        # Constraints
+        constraints = []
+
+        for j in range(len(self.feasibilityCut['W'])):
+            W = self.feasibilityCut['W'][j]
+            miu = self.feasibilityCut['miu'][j]
+            obj = cp.real(cp.sum_squares(cp.diag(lambda_old[:, 0]) @ W))
+            lagrange_terms = []
+            for k in range(self.K):
+                lhs = np.sqrt(1 + 1 / self.gamma) * cp.real(
+                    self.H[:, k:k + 1].T @ cp.diag(lambda_old[:, 0]) @ W[:, k:k + 1])
+                rhs = cp.norm(lambda_old.T @ np.hstack([np.diag(self.H[:, k]) @ W, np.zeros([self.N, 1])]) +
+                              np.hstack([np.zeros([1, self.N]), np.array([[np.sqrt(self.sigmaC2)]])]), 2)
+                lagrange_terms.append(miu[k, 0] * (rhs - lhs))
+            constraints.append(eta >= obj + sum(lagrange_terms))
+
+        if len(self.infeasibilityCut['W']) != 0:
+            for j in range(len(self.infeasibilityCut['W'])):
+                W = self.infeasibilityCut['W'][j]
+                alpha = self.infeasibilityCut['alpha'][j]
+                zeta = self.infeasibilityCut['zeta'][j]
+                obj = 0
+                lagrange_terms = []
+                for k in range(self.K):
+                    lhs = np.sqrt(1 + 1 / self.gamma) * cp.real(
+                        self.H[:, k:k + 1].T @ cp.diag(lambda_old[:, 0]) @ W[:, k:k + 1])
+                    rhs = cp.norm(lambda_old.T @ np.hstack([np.diag(self.H[:, k]) @ W, np.zeros([self.N, 1])]) +
+                                  np.hstack([np.zeros([1, self.N]), np.array([[np.sqrt(self.sigmaC2)]])]), 2)
+                    lagrange_terms.append(zeta[k, 0] * (rhs - lhs - alpha[k, 0]))
+                constraints.append(0 >= obj + sum(lagrange_terms))
+
+        # Define the objective function
+        objective = cp.Minimize(eta)
+
+        # Solve the problem
+        problem = cp.Problem(objective, constraints)
+        problem.solve(solver=cp.MOSEK, verbose=verbose)
+
+        if problem.status == cp.OPTIMAL:
+            return lambda_old.value, eta.value
+        else:
+            raise Exception('Master Problem is Infeasible!')
+
+
+if __name__ == '__main__':
+    solver = GBDSolver(Parameters)
+    print(solver.prime_problem_solver(verbose=True))
